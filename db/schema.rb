08fcbb25bd9d8f4670c5d810c44218f55e7b03c0
Matthew Bayliss <dark.arc@gmail.com>
2015-04-02 21:24:58 +0100

    Made the multilink view prettier
    The multievent view used the entire body
    of the event to display, this was a bit silly
    this has now been replaced with tagline and
    a helper which will generate a more descriptive
    text.
    
    The tagline will need adding to forms later.

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030133953) do
+ActiveRecord::Schema.define(version: 20150402195459) do
 
-  create_table "case_files", force: true do |t|
+  create_table "case_files", force: :cascade do |t|
     t.string   "name"
     t.string   "brief"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "person_event_links", force: true do |t|
+  create_table "event_types", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.integer  "case_file_id"
+		t.string   "body"
+		t.string   "tagline"
+    t.date     "date"
+    t.time     "time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "events", ["case_file_id"], name: "index_events_on_case_id"
+  add_index "events", ["tagline"], name: "index_events_on_tagline"
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "person_event_links", force: :cascade do |t|
     t.integer  "event_id"
     t.integer  "person_id"
     t.datetime "created_at"
@@ -29,26 +52,5 @@ ActiveRecord::Schema.define(version: 20141030133953) do
   end
 
   add_index "person_event_links", ["event_type_id"], name: "index_event_links_on_event_types_id"
-
-  create_table "event_types", force: true do |t|
-    t.string "name"
-  end
-
-  create_table "events", force: true do |t|
-    t.integer  "case_file_id"
-    t.string   "body"
-    t.date     "date"
-    t.time     "time"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "events", ["case_file_id"], name: "index_events_on_case_id"
-
-  create_table "people", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
