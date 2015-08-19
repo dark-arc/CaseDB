b6469cc36daba497ab708024509aee88d8a8e4c6
Matthew Bayliss <dark.arc@gmail.com>
2015-08-19 01:58:08 +0100

    Added model tests

--- test/models/case_file_test.rb
+++ test/models/case_file_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class CaseFileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Name validation" do
+    c = CaseFile.new
+    assert c.invalid?, "CaseFile can be saved without name"
+    c.name = "a"*4
+    assert c.invalid?, "Name cannot be less than 4 chars"
+    c.name = "a"*51
+    assert c.invalid, "Name can be over 50 chars"
+  end
+
+  test "Brief validation" do
+    c = CaseFile.new
+    assert c.invalid?, "Casefile can be saved without brief"
+  end
+
 end
