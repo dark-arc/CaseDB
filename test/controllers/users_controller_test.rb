385c3724f7b03657d7f4b74a29162f03c4f1b47b
Matthew Bayliss <dark.arc@gmail.com>
2015-08-24 21:56:43 +0100

    Added basic tests, nothing really working yet.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "permissions (user)" do
+     user = users(:user)
+     get :index
+     assert_response :success
+     get(:edit, {:id => user.id})
+     assert_response :success, "User can edit self"
+   end
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
   test "Person may only have one birth Event" do
-    person = Person.new
-    person.name = "a"*5
-    person.birth << Event.new
+    person = Person.first
+    person.birth = []
+    person.birth << events(:birth)
     assert person.valid?, "Person must be able to have one birth event"
-    person.birth << Event.new
+    person.birth << events(:death)
     assert person.invalid?, "Cannot have multiple birth events"
   end
   test "Person may only have one death Event" do
-    person = Person.new
-    person.name = "a"*5
-    person.death << Event.new
+    person = Person.first
+    person.death = []
+    person.death << events(:death)
     assert person.valid?, "Person must be able to have one death event"
-    person.death << Event.new
+    person.death << events(:birth)
     assert person.invalid?, "Cannot have multiple death events"
   end
 end
