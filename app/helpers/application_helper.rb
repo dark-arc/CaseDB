abe7e4972959731debb74387b04b8dd8b32c2e48
Matthew Bayliss <dark.arc@gmail.com>
2015-08-27 22:55:13 +0100

    Updated styles again.  - Cleaned up the Gemfile  - Added the ability to change the css file from within the application (to handle multiple skins)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  
+  # Initializes an instance of markdown and returns it. This should
+  # never be called directly as creating the renderer will slow down
+  # the application, use [markdown] instead.
+  #
+  # @return [Redcarpet::Markdown] markdown renderer.
   def initMarkdown()
     options = {
       filter_html: true,
@@ -14,17 +18,27 @@ module ApplicationHelper
     renderer = Redcarpet::Render::HTML.new(options)
     return Redcarpet::Markdown.new(renderer, extensions)
   end
-  
+  # Renders a piece of text given in the parameters as markdown
+  # code. This also calls initMarkdown and caches the renderer.
+  #
+  # @param text [String] Any string, preferably one with markdown in it.
+  # @return [String] Markdown formatted string
   def markdown(text)
     @markdown ||= initMarkdown
     @markdown.render(text).html_safe unless
       text == nil
   end
 
+  # Returns the image tag for an icon.
+  #
+  # @param name [String] The name of the icon
+  # @param alt [Alt] The alt text for the icon. What does the icon do?
+  # @return [String] Image tag for the icon
   def icon(name,alt)
     image_tag("icons/#{name}.svg", alt: alt, class: 'icon' )
   end
 
+  # @return [Boolean] true when there is a user signed in. 
   def signed_in?
     if session[:user_id] != nil
       true
@@ -32,9 +46,28 @@ module ApplicationHelper
       false
     end
   end
-
+  
+  # @return [String] the name of the current user
   def current_user
     @current_user ||= User.find_by_id session[:user_id]
   end
-  
+
+  # Gets the title of the current page. This is used by the layout
+  # engine and probably shouldn't be called elsewhere.
+  #
+  # @return the page title
+  def get_title
+    title = "CaseDB"
+    if content_for? :title
+      title+= " #{content_for :title}"
+    end
+  end
+
+  # Returns the current style. This is used by the layout engine and
+  # shouldn't be called elsewhere.
+  #
+  # @return the current style for this user
+  def get_style
+    return 'themes/wine'
+  end
 end
