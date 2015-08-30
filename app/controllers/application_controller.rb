5eefbf7fa140b4f0ba162e760974e0e8c05f0e40
Matthew Bayliss <dark.arc@gmail.com>
2015-08-30 17:03:35 +0100

    Fixed #47 - Controllers will redirect to index when a show page fails to find an item

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   rescue_from CanCan::AccessDenied do |e|
     redirect_to root_url, :alert => e.message
   end
 
-  #  before_filter do
-  #    resource = controller_name.singularize.to_sym
-  #    method = "#{resource}_params"
-  #    params[resource] &&= send(method) if respond_to?(method, true)
-  #  end
-
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    flash[:alert] = e.message
+    redirect_to action: :index
+  end
+  
   def current_user
     User.find_by_id session[:user_id]
   end
