cb0ce4cfb24ac82f88f55eeb1788fe09e3cff1b8
Matthew Bayliss <dark.arc@gmail.com>
2015-09-03 20:55:10 +0100

    Home controller routing tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get about" do
-    get :about
-    assert_response :success
+  ## Section : Routing
+  test "Routing: Home page" do
+    assert_routing '/',
+                   controller: "home",
+                   action: "index"
+    assert_routing '/index',
+                   controller: "home",
+                   action: "index"
   end
 
+  test "Routing: About page" do
+    assert_routing '/about',
+                   controller: "home",
+                   action: "about"
+  end
 end
