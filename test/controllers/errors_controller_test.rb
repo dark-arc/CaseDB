5b739517d875b40dc3782aab15f85999c88e10f6
Matthew Bayliss <dark.arc@gmail.com>
2015-11-14 12:17:20 +0000

    Fixed usage of class variables in test

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -1,19 +1,22 @@
 require 'test_helper'
 
 class ErrorsControllerTest < ActionController::TestCase
-  @@errorController = {
-    controller: "errors",
-    action: "show",
-    format: :html
-  }
 
+  def errorController
+    {
+      controller: "errors",
+      action: "show",
+      format: :html
+    }
+  end
+    
   test "404" do
-    assert_routing '/404', @@errorController.merge({code: 404})
+    assert_routing '/404', errorController.merge({code: 404})
   end
   test "422" do
-    assert_routing '/422', @@errorController.merge({code: 422})
+    assert_routing '/422', errorController.merge({code: 422})
   end
   test "500" do
-    assert_routing '/500', @@errorController.merge({code: 500})
+    assert_routing '/500', errorController.merge({code: 500})
   end
 end
