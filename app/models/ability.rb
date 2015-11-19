e787f4939b5f61bb176564766d58bcdbe7f059d7
Matthew Bayliss <dark.arc@gmail.com>
2015-11-19 21:40:18 +0000

    Fixed #97 - Seperated abilities into methods per role.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,26 +11,33 @@ class Ability
     user ||= User.new
     
     can :read, @coreModels
-    can [:show, :update, :destroy], User, :id => user.id
     
-    if user.new_record?
+    load_unauthenticated_permissions if user.new_record?
+    load_user_permissions if user.user?
+    load_moderator_permissions if user.moderator?
+    load_researcher_permissions if user.researcher?
+    load_admin_permissions if user.admin?
+  end
+
+  private
+
+  def load_unauthenticated_permissions
       can :create, User
-    end
-
-    if user.user?
-      
-    end
-    
-    if user.moderator?
-      can [:promote,:show,:update,:create,:destroy], @userModels
-    end
-
-    if user.researcher?
-      can [:update,:create,:destroy], @coreModels
-    end
-
-    if user.admin?
+  end
+  
+  def load_admin_permissions
       can :manage, :all
-    end
+  end
+
+  def load_moderator_permissions
+      can [:promote,:show,:update,:create,:destroy], @userModels
+  end
+
+  def load_researcher_permissions
+      can [:update,:create,:destroy], @coreModels
+  end
+
+  def load_user_permissions
+    can [:show, :update, :destroy], User, :id => user.id
   end
 end
