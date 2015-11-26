947e305ba8dd78de1ee98af37b3f9826fa8e0c87
Matthew Bayliss <dark.arc@gmail.com>
2015-11-26 20:49:03 +0000

    Removed old marks model

--- test/factories/marks.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :mark do
-    
-  end
-
-end
--- test/models/mark_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MarkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
