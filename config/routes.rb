ba29e734e22585bcb024f69c19153f697105418b
Matthew Bayliss <dark.arc@gmail.com>
2015-11-27 00:02:31 +0000

    Implemented mark types

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get	'index', to: 'home#index'
   get	'about', to: 'home#about'
+
   [404,422,500].each do |code|
     get "#{code}", :to => "errors#show", :code => code, :defaults => {:format => :html}
   end
@@ -12,7 +13,9 @@ Rails.application.routes.draw do
   resources :people do
     resources :marks
   end
+  # Configurables
   resources :anatomies
+  resources :mark_types
   
   resources :users 
   resources :session, only: [:new,:create,:destroy]
