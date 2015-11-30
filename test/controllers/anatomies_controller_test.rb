8b21af68557f9524b4ccb8fe1cacfeea7064106f
Matthew Bayliss <dark.arc@gmail.com>
2015-11-30 20:09:30 +0000

    Added tests for anatomies

--- test/controllers/anatomies_controller_test.rb
+++ test/controllers/anatomies_controller_test.rb
@@ -2,45 +2,63 @@ require 'test_helper'
 
 class AnatomiesControllerTest < ActionController::TestCase
   setup do
-    @anatomy = anatomies(:one)
+    @anatomy = build :anatomy
+    @params = { user_id: create(:user, :admin).id }
   end
 
-  test "should get index" do
-    get :index
+  test 'should get index' do
+    get :index, session: @params
     assert_response :success
   end
 
-  test "should get new" do
-    get :new
+  test 'should get new' do
+    get :new, session: @params
     assert_response :success
   end
 
-  test "should create anatomy" do
+  test 'should create anatomy' do
     assert_difference('Anatomy.count') do
-      post :create, params: { anatomy: {  } }
+      post :create,
+           params: {
+             anatomy: { name: @anatomy.name }
+           }, session: @params
     end
 
     assert_redirected_to anatomy_path(Anatomy.last)
   end
 
-  test "should show anatomy" do
-    get :show, params: { id: @anatomy }
+  test 'should show anatomy' do
+    @anatomy.save
+    get :show,
+        params: { id: @anatomy },
+        session: @params
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, params: { id: @anatomy }
+  test 'should get edit' do
+    @anatomy.save
+    get :edit,
+        params: { id: @anatomy },
+        session: @params
     assert_response :success
   end
 
-  test "should update anatomy" do
-    patch :update, params: { id: @anatomy, anatomy: {  } }
+  test 'should update anatomy' do
+    @anatomy.save
+    patch :update,
+          params: {
+            id: @anatomy,
+            anatomy: { name: 'test' } },
+          session: @params
     assert_redirected_to anatomy_path(@anatomy)
   end
 
-  test "should destroy anatomy" do
+  test 'should destroy anatomy' do
+    @anatomy.save
     assert_difference('Anatomy.count', -1) do
-      delete :destroy, params: { id: @anatomy }
+      delete :destroy,
+             params: { id: @anatomy },
+             session: @params
     end
 
     assert_redirected_to anatomies_path
--- test/factories/anatomies.rb
+++ test/factories/anatomies.rb
@@ -1,11 +1,5 @@
 FactoryGirl.define do
   factory :anatomy do
-    name "MyString"
-parent_id 1
-lft 1
-rgt 1
-depth 1
-children_count 1
+    name { Faker::Internet.slug }
   end
-
 end
