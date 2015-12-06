ea8c19c801a2eaa83ec7410d2d953f32ace2a2e0
Matthew Bayliss <dark.arc@gmail.com>
2015-12-06 23:54:17 +0000

    Rubocop cleaning the files modified in the last two commits.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,20 +1,19 @@
+# Helper methods for people views
 module PeopleHelper
-
   def new_personal_event_path(person, type = :birth)
     new_event_path(
-      :event => {
-        :event_people_attributes => {
-          "0" => {
+      event: {
+        event_people_attributes: {
+          '0' => {
             person_id: person,
-            :category => type
+            category: type
           }
         }
       }
     )
   end
 
-  def edit_mark_link(person,mark)
-    link_to(t('.editMark'), edit_person_mark_path(person,mark))
+  def edit_mark_link(person, mark)
+    link_to(t('.editMark'), edit_person_mark_path(person, mark))
   end
-  
 end
--- app/models/mark_type.rb
+++ app/models/mark_type.rb
@@ -1,11 +1,12 @@
+# This is a semi-static list of all possible mark types
 class MarkType < ActiveRecord::Base
   acts_as_nested_set
   def path
-    path = ""
+    path = ''
     ancestors.each do |anc|
       path += "#{anc.name}/"
     end
     path += name
-    return path
+    path
   end
 end
