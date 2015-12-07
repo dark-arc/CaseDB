1a290901ace3bed38cb2194e7d80e93b89cabaab
Matthew Bayliss <dark.arc@gmail.com>
2015-12-07 00:38:17 +0000

    Updated tests for people

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,28 +1,65 @@
 require 'test_helper'
-
+# Tests for people controller
 class PeopleControllerTest < ActionController::TestCase
-  test "Guest can view index" do
-    get :index
+  setup do
+    @person = build :person
+    @params = { user_id: create(:user, :admin).id }
+  end
+
+  test 'should get index' do
+    get :index, session: @params
     assert_response :success
   end
-  test "Guest can view show" do 
-    get :show, params: {id: create(:person).id}
-    assert_response :success
-    assert_not_nil assigns(:person)
-  end
-  test "Guest cannot edit" do 
-    get :edit, params: {id: create(:person).id}
-    assert_response 403
-    assert_not_nil assigns(:message)
-  end
-  test "Researcher can edit" do
-    get :edit, params: {id: create(:person).id},
-        session: {user_id: create(:user,:researcher).id}
+
+  test 'should get new' do
+    get :new, session: @params
     assert_response :success
   end
-  test "Invalid ID shows index" do
-    get :show, params: {id: 5000}
-    assert_response :redirect
-    assert_not flash[:alert].empty?
+
+  test 'should create person' do
+    assert_difference('Person.count') do
+      post :create,
+           params:  { person: @person.attributes },
+           session: @params
+    end
+
+    assert_redirected_to person_path(Person.last)
+  end
+
+  test 'should show person' do
+    @person.save
+    get :show,
+        params: { id: @person },
+        session: @params
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    @person.save
+    get :edit,
+        params: { id: @person },
+        session: @params
+    assert_response :success
+  end
+
+  test 'should update person' do
+    @person.save
+    patch :update,
+          params: {
+            id: @person,
+            person: { name: 'test' } },
+          session: @params
+    assert_redirected_to person_path(@person)
+  end
+
+  test 'should destroy person' do
+    @person.save
+    assert_difference('Person.count', -1) do
+      delete :destroy,
+             params: { id: @person },
+             session: @params
+    end
+
+    assert_redirected_to people_path
   end
 end
