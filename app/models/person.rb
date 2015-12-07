cdc0bf85a35cbdcd594ccbf408f2c0194a76ef00
Matthew Bayliss <dark.arc@gmail.com>
2015-12-07 22:33:15 +0000

    Refactored person model (placed enums in seperate file)

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,16 +1,17 @@
 # Person class contains the descriptions of people and a narative of
-# their life. 
+# their life.
 class Person < ActiveRecord::Base
+  include PersonEnum
   has_many :event_people
-  #@attribute events
+  # @attribute events
   # @return [Relation<Event>] Narrative for this persons life
   has_many :events,
-           :through => :event_people
-  #@attribute case_files 
+           through: :event_people
+  # @attribute case_files
   # @return [Relation<CaseFile>] Cases this person was involved in
   has_many :case_files,
-           -> {distinct},
-           :through => :events
+           -> { distinct },
+           through: :events
   validate :validate_birth_event_count,
            :validate_death_event_count
   attr_accessor :name
@@ -18,27 +19,11 @@ class Person < ActiveRecord::Base
   accepts_nested_attributes_for :aliases,
                                 allow_destroy: true
   has_many :marks
-  
-  def name
-    begin
-      aliases.default_name.first.name
-    rescue
-      aliases.first.name
-    end
-  end
 
-  def name=(nom)
-    a = aliases.
-        find_or_create_by(
-          person: self,
-          name: nom
-        )
-    aliases.default_name.each do |p|
-      p.default = false
-      p.save
-    end
-    a.default = true
-    a.save
+  def name
+    aliases.default_name.first.name
+  rescue
+    aliases.first.name
   end
 
   def dob
@@ -56,156 +41,55 @@ class Person < ActiveRecord::Base
       return death.first.date
     end
   end
-  #@attribute birth
+  # @attribute birth
   # @return [CollectionProxy<Event>] birth event of this person
-  
-  #@attribute death
+
+  # @attribute death
   # @return [CollectionProxy<Event>] death event of this person
-  
-  #@attribute victim
+
+  # @attribute victim
   # @return [CollectionProxy<Event>] events where this person was
   # a victim
-  
-  #@attribute perpetrator
+
+  # @attribute perpetrator
   # @return [CollectionProxy<Event>] events where this person was
   # a perpetrator
-  
-  #@attribute investigator
+
+  # @attribute investigator
   # @return [CollectionProxy<Event>] events where this person was
-  # an investigator  
+  # an investigator
   EventPerson.categories.each do |type|
     type = type[0].to_sym
     has_many type,
-             -> {where 'event_people.category' => type},
-             :through => :event_people,
-             :source => :event
+             -> { where 'event_people.category' => type },
+             through: :event_people,
+             source: :event
   end
 
-  #@!attribute gender
-  # @return [Symbol] The gender of this person. 
-  enum gender: [
-         :unknown,
-         :male,
-         :female,
-         :asexual,
-         :transmale,
-         :transfemale
-       ], _prefix: true
-  #@!attribute eye
-  # @return [Symbol] The eye colour of this person. 
-  enum eye_colour: [
-         :unknown,
-         :lightBlue,
-         :blue,
-         :blueGray,
-         :gray,
-         :blueGrayBrown,
-         :grayGreenBrown,
-         :green,
-         :greenBrown,
-         :lightBrown,
-         :brown,
-         :hazel,
-         :darkBrown,
-         :black
-       ] , _suffix: 'eyes'
-  #@!attribute height
-  # @return [Symbol] The height of this person. 
-  enum height: [
-         :unknown,
-         :dwarf,
-         :short,
-         :average,
-         :tall
-       ], _prefix:true
-  #@!attribute weight
-  # @return [Symbol] The weight of this person. 
-  enum build: [
-         :unknown,
-         :slight,
-         :average,
-         :overweight,
-         :obese
-       ], _prefix:true
-  #@!attribute hair_colour
-  # @return [Symbol] The hair_colour of this person. 
-  enum hair_colour: [
-         :unknown,
-         :black,
-         :brown,
-         :blond,
-         :auburn,
-         :chestnut,
-         :red,
-         :gray
-       ], _suffix: true
-  #@!attribute hair_length
-  # @return [Symbol] The hair_length of this person. 
-  enum hair_length: [
-         :unknown,
-         :bald,
-         :short,
-         :medium,
-         :shoulderLength,
-         :backLength
-       ], _suffix: true
-  #@!attribute moustache
-  # @return [Symbol] The type of moustache the person had
-  # @todo add types of moustache
-  enum moustache: [
-         :unknown,
-         :none,
-         :handleBar,
-         :toothbrush,
-       ],_prefix: true
-  #@!attribute gender
-  # @return [Symbol] The type of beard the person had
-  # @todo add types of beard
-  enum beard: [
-         :unknown,
-         :clean,
-         :stubble,
-         :soulPatch,
-         :full,
-         :muttonChops,
-         :long,
-         :medium
-       ],_prefix: true
-  #@!attribute ics
-  # @return [Symbol] The ICS code of the person
-  enum ic: [
-         :unknown,
-         :northEurope,
-         :southEurope,
-         :black,
-         :westAsia,
-         :eastAsia,
-         :arabic
-       ], _prefix: true
+  def description_fields
+    [:ic, :gender, :height, :build, :eye_colour,
+     :hair_length, :hair_colour, :beard, :moustache]
+  end
+
   def description
-    @description_fields ||=
-      [:ic,:gender,:height,:build,:eye_colour,
-       :hair_length, :hair_colour,:beard,:moustache]
     description = {}
-    @description_fields.each do |v|
-      attr = read_attribute v
+    description_fields.each do |v|
+      attr = self[v]
       next if attr == 'unknown' || attr.nil?
       field = Person.human_attribute_name(v)
       attr = Person.human_attribute_name(attr)
       description[field] = attr
     end
-    return description
+    description
   end
-  private 
+
+  private
+
   def validate_birth_event_count
-    if self.birth.size > 1
-      errors.add(:birth, :multipleBirthEvents)
-    end
+    errors.add(:birth, :multipleBirthEvents) if birth.size > 1
   end
 
   def validate_death_event_count
-    if self.death.size > 1
-      errors.add(:death, :multipleDeathEvents)
-    end
+    errors.add(:death, :multipleDeathEvents) if death.size > 1
   end
 end
---
+++ app/models/person_enum.rb
@@ -0,0 +1,104 @@
+# Enums for Person module
+module PersonEnum
+  # @!attribute gender
+  # @return [Symbol] The gender of this person.
+  Person.enum gender: [
+    :unknown,
+    :male,
+    :female,
+    :asexual,
+    :transmale,
+    :transfemale
+  ], _prefix: true
+  # @!attribute eye
+  # @return [Symbol] The eye colour of this person.
+  Person.enum eye_colour: [
+    :unknown,
+    :lightBlue,
+    :blue,
+    :blueGray,
+    :gray,
+    :blueGrayBrown,
+    :grayGreenBrown,
+    :green,
+    :greenBrown,
+    :lightBrown,
+    :brown,
+    :hazel,
+    :darkBrown,
+    :black
+  ], _suffix: 'eyes'
+  # @!attribute height
+  # @return [Symbol] The height of this person.
+  Person.enum height: [
+    :unknown,
+    :dwarf,
+    :short,
+    :average,
+    :tall
+  ], _prefix: true
+  # @!attribute weight
+  # @return [Symbol] The weight of this person.
+  Person.enum build: [
+    :unknown,
+    :slight,
+    :average,
+    :overweight,
+    :obese
+  ], _prefix: true
+  # @!attribute hair_colour
+  # @return [Symbol] The hair_colour of this person.
+  Person.enum hair_colour: [
+    :unknown,
+    :black,
+    :brown,
+    :blond,
+    :auburn,
+    :chestnut,
+    :red,
+    :gray
+  ], _suffix: true
+  # @!attribute hair_length
+  # @return [Symbol] The hair_length of this person.
+  Person.enum hair_length: [
+    :unknown,
+    :bald,
+    :short,
+    :medium,
+    :shoulderLength,
+    :backLength
+  ], _suffix: true
+  # @!attribute moustache
+  # @return [Symbol] The type of moustache the person had
+  # @todo add types of moustache
+  Person.enum moustache: [
+    :unknown,
+    :none,
+    :handleBar,
+    :toothbrush
+  ], _prefix: true
+  # @!attribute gender
+  # @return [Symbol] The type of beard the person had
+  # @todo add types of beard
+  Person.enum beard: [
+    :unknown,
+    :clean,
+    :stubble,
+    :soulPatch,
+    :full,
+    :muttonChops,
+    :long,
+    :medium
+  ], _prefix: true
+  # @!attribute ics
+  # @return [Symbol] The ICS code of the person
+  Person.enum ic: [
+    :unknown,
+    :northEurope,
+    :southEurope,
+    :black,
+    :westAsia,
+    :eastAsia,
+    :arabic
+  ], _prefix: true
+end
