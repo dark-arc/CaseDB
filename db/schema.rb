868fa7956b0ec7186f8f41446afe3d4b27cbed87
Matthew Bayliss <dark.arc@gmail.com>
2016-02-28 20:38:56 +0000

    Added tagline to case_file table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228182202) do
+ActiveRecord::Schema.define(version: 20160228203658) do
 
   create_table "aliases", force: :cascade do |t|
     t.integer  "person_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160228182202) do
     t.string   "brief"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "tagline"
   end
 
   create_table "event_people", force: :cascade do |t|
